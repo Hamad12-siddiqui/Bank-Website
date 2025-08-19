0ed275c612c4207535dad4a9ddf6e00c0029dc1c
Hamad12-siddiqui <hamadahamd7765@gmail.com>
2025-08-19 16:31:48 +0500

    get pull

---
+++ app/component/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Image from 'next/image'
+const Footer = () => {
+  return (
+    <>
+    <Image 
+    src = "/images/shape-30"
+    alt = "logo"
+    width={100}
+    height={100}
+    />
+    </>
+  )
+}
+
+export default Footer
