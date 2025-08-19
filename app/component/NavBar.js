345ff6749b181f613195b1eba4c5c45f252ce02f
Usama Afzal <usamaafzal489@gmail.com>
2025-08-19 17:08:54 +0500

    set bydefult black color

--- app/component/NavBar.js
+++ app/component/NavBar.js
@@ -3,14 +3,14 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div className='w-full flex justify-center items-center'>
-        <div className='w-[85%] border-2  '>
-            <div className=''>
+        <div className='w-[85%] border-2 flex justify-between items-center'>
+            <div className=' flex items-center gap-2'>
                 <img
                 src="/images/shape-30.svg"
                 alt="Logo"
-                className='w-12 h-12'
+                className='w-10 h-10'
                 />
-                <h3>your Bank</h3>
+                <h3 className='text-lg font-semibold'>your Bank</h3>
             </div>
 
         </div>
