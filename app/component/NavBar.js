d6f2b4a250af956a3b005ec4d03b03af97769f1c
Usama Afzal <usamaafzal489@gmail.com>
2025-08-19 16:35:15 +0500

    change the branch

--- app/component/NavBar.js
+++ app/component/NavBar.js
@@ -4,6 +4,14 @@ const NavBar = () => {
   return (
     <div className='w-full flex justify-center items-center'>
         <div className='w-[85%] border-2  '>
+            <div className=''>
+                <img
+                src="/images/shape-30.svg"
+                alt="Logo"
+                className='w-12 h-12'
+                />
+                <h3>your Bank</h3>
+            </div>
 
         </div>
     </div>
