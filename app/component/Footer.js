7559b43b954835aa699373f392407acbab28876e
Hamad12-siddiqui <hamadahamd7765@gmail.com>
2025-08-19 18:07:45 +0500

    Enhance Footer component with contact information and update dependencies for react-icons

--- app/component/Footer.js
+++ app/component/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
+import { FaPhone } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { TbMessageCircleFilled } from "react-icons/tb";
+
 const Footer = () => {
   return (
     <>
-    <div className='flex gap-2 '>
+    <div className='max-w-7xl mx-auto'>
+    <div className='border-b '>
+    <div className='flex gap-2  justify-center  '>
     <Image 
     src = "/images/shape-30.svg"
     alt = "logo"
@@ -15,8 +21,8 @@ const Footer = () => {
     </h1>
      </div>
     <nav>
-        <ul className='flex gap-8 text-white'>
-            <li>
+        <ul className='flex gap-8 text-white justify-center my-8  '>
+            <li className=''>
                 Home
             </li>
            <li> Careers</li>
@@ -24,7 +30,22 @@ const Footer = () => {
            <li>Security</li>
         </ul>
     </nav>
-   
+   </div>
+   <div className='flex gap-4 justify-center mt-9'>
+   <div className='flex gap-1 '>
+  <TbMessageCircleFilled className='text-[#CAFF33]' size={20}/>
+  <p className='text-white'>hello@skillbirdge.com</p>
+   </div>
+   <div className='flex gap-1'>
+    <FaPhone className='text-[#CAFF33]' size={20}/>
+    <p className='text-white'>1234567890</p>
+   </div>
+   <div className='flex gap-1'>
+    <FaLocationDot className='text-[#CAFF33]' size={20}/>
+    <p className='text-white'>Somewhere in the World</p>
+   </div>
+   </div>
+   </div>
     </>
   )
 }
